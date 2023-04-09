b13da454b38ff58190fee27b0dcd488cc76a0a52
imdaredevil <cibi_16@live.com>
2023-04-09 12:44:46 -0700

    get doc

--- Backend/main.py
+++ Backend/main.py
@@ -5,6 +5,7 @@ from apis.store import Store
 from apis.query import Query
 from apis.get_titles import GetTitles
 from apis.upload import Upload
+from apis.get_document import GetDocument
 
 app = Flask(__name__)
 cors = CORS(app, resource={
@@ -18,5 +19,6 @@ api.add_resource(Store, '/store')
 api.add_resource(Query,'/query')
 api.add_resource(GetTitles,'/get_titles')
 api.add_resource(Upload, '/upload')
+api.add_resource(GetDocument, '/get_document')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
