6f38460f525dcf4f13668df45f5b89d8fc5490fe
Bhargav Krishnamurthy <bhargavkrish@Bhargavs-MacBook-Pro.local>
2023-04-09 04:40:11 -0700

    Added parsing logic for getting source and getting doc ids in query file

--- Backend/apis/query.py
+++ Backend/apis/query.py
@@ -6,12 +6,28 @@ import os
 from llama_index import GPTSimpleVectorIndex
 from llama_index import Document
 import json
+import re
+
 
 os.environ["OPENAI_API_KEY"] = "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"
 openai.organization = "org-Ddi6ZSgWKe8kPZlpwd6M6WVe"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
+def retrieveKeystore() -> dict:
+    json_path = "/Users/bhargavkrish/Desktop/USC/hackathon/Own-Your-Terms/Backend/database.json"
+    with open(json_path,'r') as j:
+        database_json = json.loads(j.read())
+    if "vector_store" in database_json:
+        a = database_json["vector_store"]
+        if "__data__" in a:
+            b = a["__data__"]
+            if "simple_vector_store_data_dict" in b:
+                c = b["simple_vector_store_data_dict"]
+                if "text_id_to_doc_id" in c:
+                    d = c["text_id_to_doc_id"]
+                    return d
+    return {}
 
 
 class Query(Resource):
@@ -22,7 +38,28 @@ class Query(Resource):
         if os.path.exists("database.json"):
             existing_index = GPTSimpleVectorIndex.load_from_disk('database.json')
             res = existing_index.query(data)
-            print(res.get_formatted_sources(length=1000))
+            print(res.get_formatted_sources(length=10000))
+            sources = res.get_formatted_sources(length=10000)
+            text_id_strings = re.findall(r'\((.*?)\)',sources)
+            print(text_id_strings)
+            text_ids = []
+            doc_ids = []
+            for text_id_string in text_id_strings:
+                if ":" in text_id_string:
+                    text_id = text_id_string.split(":")[1]
+                    text_ids.append(text_id)
+                    print(text_id)
+            keystore = retrieveKeystore()
+            print(keystore)
+            for t in text_ids:
+                print(t)
+                t = t.lstrip(" ")
+                t = t.rstrip(" ")
+                if t in keystore:
+                    print("Keystore: "+keystore[t])
+                    doc_ids.append(keystore[t])
+                            
+            print(doc_ids)
             return str(res),200
         else:
             return "Database not found", 404
