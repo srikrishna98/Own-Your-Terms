69f5fa980b7461614bc61b742782ab1e217afe30
srikrishna98 <srikrishna0412@gmail.com>
2023-04-09 05:15:43 -0700

    Extensions completion

---
+++ Extension/scripts/background.js
@@ -0,0 +1,18 @@
+"use strict";
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  console.log("previousVersion", details.previousVersion);
+});
+
+// this sets the icon text in the chrome toolbar
+//chrome.browserAction.setBadgeText({ text: 'TOSS' });
+
+// function sendSummary(info, tab) {
+//   chrome.tabs.sendMessage(tab.id, {from: 'contextMenu', subject: info});
+// }
+
+// chrome.contextMenus.create({
+//   title: 'TOS Summary',
+//   contexts:['selection'],
+//   onclick: sendSummary,
+// });
