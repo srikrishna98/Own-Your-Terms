5415c62670ef72014c4bb2b30f56cd5008af7c33
srikrishna98 <srikrishna0412@gmail.com>
2023-04-08 17:53:10 -0700

    Folder structure created

---
+++ Backend/main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_restx import Api
+from apis.store import Store
+
+app = Flask(__name__)
+cors = CORS(app, resource={
+    r"/*": {
+        "origins": "*"
+    }
+})
+
+api = Api(app, version='1.0')
+api.add_resource(Store, '/store')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ Frontend/readme.md
@@ -0,0 +1,1 @@
+## Frontend King SHRINIVASHAN
